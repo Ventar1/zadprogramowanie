ac6736f4903b9d960549b1104775bdfb2b8ad87b
Ventar1 <164664072+Ventar1@users.noreply.github.com>
2024-04-24 21:03:42 +0200

    Update ZAD1.py

--- Python_Conditional_Statements_Loops/ZAD1.py
+++ Python_Conditional_Statements_Loops/ZAD1.py
@@ -4,10 +4,10 @@ degree = int(temp[:-1])
 i_convention = temp[-1]
 if i_convention.upper() == "C":
     result = int(round((9 * degree) / 5 + 32))
-    o_convention = "Fahrenheit"  # Set the output convention as Fahrenheit
+    o_convention = "Fahrenheit" 
 elif i_convention.upper() == "F":
     result = int(round((degree - 32) * 5 / 9))
-    o_convention = "Celsius"  # Set the output convention as Celsius
+    o_convention = "Celsius" 
 else:
     print("Input proper convention.")
     quit()
